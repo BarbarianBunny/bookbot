cf0792e6fa5e9ff9b47c2db5938116fa92a7b7df
BarbarianBunny <carury@gmail.com>
2025-01-04 19:14:38 -0800

    update main.py prints word count

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("hello world")
+def main():
+    book_name = "frankenstein"
+    print_word_count()
+
+def get_text(book_name):
+    book_path = "books/" + book_name.lower() + ".txt"
+    with open(book_path) as f:
+        return f.read()
+
+def get_word_count(text):
+    words = text.split()
+    return len(words)
+
+def print_word_count(book_name):
+    text = get_text(book_name)
+    word_count = get_word_count(text)
+    print(f"{book_name.title()} has {word_count} words.")
+
+
+main()
