594a5504cba9ee8d24b3b8d097c3f08cdfb99088
BarbarianBunny <carury@gmail.com>
2025-01-04 19:55:21 -0800

    update main.py book report

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
+import string
+
 def main():
     book_name = "frankenstein"
-    print_word_count(book_name)
     text = get_text(book_name)
+    word_count = get_word_count(text)
     character_counts = get_character_counts(text)
-    print_sorted_dict(character_counts)
+    print_book_report(book_name, word_count, character_counts)
     
+def print_book_report(book_name, word_count, character_counts):
+    print(f"--- Begin report of {book_name} ---\n")
+    print(f"{word_count} words found in the document\n")
+    print("Letters found:")
+    for key, value in sorted(character_counts.items()):
+        if key in string.ascii_lowercase:
+            print(f"The '{key}' character was found {value} times")
+    print()
+    print("Numbers found:")
+    for key, value in sorted(character_counts.items()):
+        if key in string.digits:
+            print(f"The '{key}' character was found {value} times")
+
 def get_character_counts(text):
     text = text.lower()
     chars = {}
@@ -12,11 +27,12 @@ def get_character_counts(text):
         if char in chars:
             continue
         chars[char] = text.count(char)
+    chars.pop("\n", None)
     return chars
 
 def print_sorted_dict(dict):
     for key, value in sorted(dict.items()):
-        print(key + ": " + str(value))
+        print(key + ": " + str(value))    
 
 def get_text(book_name):
     book_path = "books/" + book_name.lower() + ".txt"
