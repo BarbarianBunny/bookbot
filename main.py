5af296452bfcfa6a6f1e19c02c05ff10516f1f92
BarbarianBunny <carury@gmail.com>
2025-01-04 19:40:06 -0800

    update main.py print char count

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 def main():
     book_name = "frankenstein"
-    print_word_count()
+    print_word_count(book_name)
+    text = get_text(book_name)
+    character_counts = get_character_counts(text)
+    print_sorted_dict(character_counts)
+    
+def get_character_counts(text):
+    text = text.lower()
+    chars = {}
+    for char in text:
+        if char in chars:
+            continue
+        chars[char] = text.count(char)
+    return chars
+
+def print_sorted_dict(dict):
+    for key, value in sorted(dict.items()):
+        print(key + ": " + str(value))
 
 def get_text(book_name):
     book_path = "books/" + book_name.lower() + ".txt"
